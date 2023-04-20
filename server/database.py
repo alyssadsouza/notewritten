653785f7096cf5e61510046fd7e3448930b11c93
alyssadsouza <alyssa.dsouza63@gmail.com>
2023-04-19 22:31:49 -0400

    prevent sql server timeout

--- server/database.py
+++ server/database.py
@@ -6,7 +6,7 @@ from sqlalchemy_cockroachdb import run_transaction
 db_uri = os.environ['DATABASE_URL'].replace("postgresql://", "cockroachdb://")
 
 try:
-	engine = create_engine(db_uri, connect_args={"application_name":"docs_simplecrud_sqlalchemy"})
+	engine = create_engine(db_uri, connect_args={"application_name":"docs_simplecrud_sqlalchemy"}, pool_pre_ping=True)
 except Exception as e:
 	print("Failed to connect to database.")
 	print(f"{e}")
